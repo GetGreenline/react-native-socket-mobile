02893148cd0d0e8ba905d445b3e0495081e97d61
SamLand <sam@getgreenline.co>
2019-11-01 16:04:29 -0700

    using stringFromDecodedData (#2)

--- ios/ReactNativeSocketMobile.m
+++ ios/ReactNativeSocketMobile.m
@@ -68,7 +68,7 @@ RCT_EXPORT_METHOD(stop: (RCTPromiseResolveBlock)resolve rejecter:(RCTPromiseReje
 
 -(void)didReceiveDecodedData:(SKTCaptureDecodedData*) decodedData fromDevice:(SKTCaptureHelperDevice*) device withResult:(SKTResult) result{
     if (SKTSUCCESS(result) && hasListeners) {
-        NSString* dataString = [NSString stringWithUTF8String:(const char *)[decodedData.DecodedData bytes]];
+         NSString* dataString = decodedData.stringFromDecodedData;
         if (dataString == nil) {
             dataString = @"";
         }
